536b58a12df417c8d997ef3548d9f2a34a47a772
MateuszWalowski <mat.walowski@gmail.com>
2020-09-10 10:33:59 +0200

    improved

--- script.js
+++ script.js
@@ -1,85 +1,89 @@
 var theInput = document.querySelector("body > input[type=color]");
-theInput.addEventListener("input", function () {
-
-    document.getElementById("hex").innerHTML = "Hex: " + theInput.value;
-    document.querySelector("body > div").style.backgroundColor = theInput.value;
-
-    function hexToRGB(color) {
-
-        let r = color.substring(1, 3)
-        let convertedred = parseInt(r, 16)
-        let g = color.substring(3, 5)
-        let convertedgreen = parseInt(g, 16)
-        let b = color.substring(5)
-        let convertedblue = parseInt(b, 16)
-        let newcolorhexToRGB = "r: " + convertedred + ", g: " + convertedgreen + ", b: " + convertedblue
-        document.getElementById("rgb").innerHTML = "RGB: " + newcolorhexToRGB;
+theInput.addEventListener("input", readvalue);
 
 
-        convertedred /= 255;
-        convertedgreen /= 255;
-        convertedblue /= 255;
-        console.log(convertedred)
-        console.log(convertedgreen)
-        console.log(convertedblue)
+function readvalue() {
+    let hexvalue = theInput.value
+    console.log(theInput.value);
+    showcolor(hexvalue);
+    showhexvalue(hexvalue);
+    hexToRGB(hexvalue);
+    return (hexvalue)
+}
 
+function showcolor(hexvalue) {
+    document.querySelector("body > div").style.backgroundColor = hexvalue;
+}
 
-        let h, s, l;
+function showhexvalue(hexvalue) {
+    document.getElementById("hex").innerHTML = "Hex: " + hexvalue;
+}
 
-        const min = Math.min(convertedred, convertedgreen, convertedblue);
-        console.log(min)
-
-        const max = Math.max(convertedred, convertedgreen, convertedblue);
-        console.log(max)
-
-        if (max === min) {
-            h = 0;
-            // console.log(h)
-        } else
-        if (max === convertedred) {
-            h = 60 * (0 + (convertedgreen - convertedblue) / (max - min));
-            // console.log(h)
-
-        } else
-        if (max === convertedgreen) {
-            h = 60 * (2 + (convertedblue - convertedred) / (max - min));
-            // console.log(h)
-
-        } else
-        if (max === convertedblue) {
-            h = 60 * (4 + (convertedred - convertedgreen) / (max - min));
-            // console.log(h)
-
-        }
-
-        if (h <= 0) {
-            h = h + 360;
-            // console.log(h)
-
-        }
-
-        l = (min + max) / 2;
-
-        if (max === 0 || min === 1) {
-            s = 0;
-        } else {
-            s = (max - l) / (Math.min(l, 1 - l));
-        }
-        // multiply s and l by 100 to get the value in percent, rather than [0,1]
-        let roundH = Math.round(h);
-
-
-        s *= 100;
-        let roundS = Math.round(s);
-        l *= 100;
-        let roundL = Math.round(l);
-
-
-        // console.log("hsl(%f,%f%,%f%)", h, roundS, roundL); // just for testing
-        document.getElementById("HSL").innerHTML = "HSL: " + roundH + "," + roundS + "," + roundL;
-
-
+function hexToRGB(hexvalue) {
+    let r = hexvalue.substring(1, 3)
+    let convertedred = parseInt(r, 16)
+    let g = hexvalue.substring(3, 5)
+    let convertedgreen = parseInt(g, 16)
+    let b = hexvalue.substring(5)
+    let convertedblue = parseInt(b, 16)
+    showRGB(convertedred, convertedgreen, convertedblue)
+    RGBtoHSL(convertedred, convertedgreen, convertedblue)
+    return {
+        convertedred,
+        convertedgreen,
+        convertedblue
     }
+}
 
-    hexToRGB(theInput.value);
-})
+function showRGB(convertedred, convertedgreen, convertedblue) {
+    let newcolorhexToRGB = "r: " + convertedred + ", g: " + convertedgreen + ", b: " + convertedblue
+    document.getElementById("rgb").innerHTML = "RGB: " + newcolorhexToRGB;
+}
+
+function RGBtoHSL(convertedred, convertedgreen, convertedblue) {
+    convertedred /= 255;
+    convertedgreen /= 255;
+    convertedblue /= 255;
+    let h, s, l;
+    const min = Math.min(convertedred, convertedgreen, convertedblue);
+    const max = Math.max(convertedred, convertedgreen, convertedblue);
+    if (max === min) {
+        h = 0;
+    } else
+    if (max === convertedred) {
+        h = 60 * (0 + (convertedgreen - convertedblue) / (max - min));
+    } else
+    if (max === convertedgreen) {
+        h = 60 * (2 + (convertedblue - convertedred) / (max - min));
+    } else
+    if (max === convertedblue) {
+        h = 60 * (4 + (convertedred - convertedgreen) / (max - min));
+    }
+    if (h <= 0) {
+        h = h + 360;
+    }
+    l = (min + max) / 2;
+    if (max === 0 || min === 1) {
+        s = 0;
+    } else {
+        s = (max - l) / (Math.min(l, 1 - l));
+    }
+    h = Math.round(h);
+    s *= 100;
+    s = Math.round(s);
+    l *= 100;
+    l = Math.round(l);
+
+    showHSL(h, s, l)
+    return {
+        h,
+        s,
+        l
+    }
+}
+
+function showHSL(h, s, l) {
+
+    document.getElementById("HSL").innerHTML = "HSL: " + h + "," + s + "," + l;
+
+}
